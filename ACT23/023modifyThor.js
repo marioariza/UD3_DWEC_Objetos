f9c324c5e639f1fcd0ada53dcc5d245567412cb0
*Mario <*mariojra6@gmail.com*>
2022-11-10 13:34:51 +0100

    Archivos actualizados

--- ACT23/023modifyThor.js
+++ ACT23/023modifyThor.js
@@ -8,17 +8,17 @@ alert('El largo de la cadena es: ' + thor.length); // 23
 
 // Encontrar la letra o
 
-let posiciones_o = [];
+let posiciones_o = []; // Creamos un array para guardar las posiciones de o.
 
-for (let i = 0; i < thor.length; i++) {
-    if (thor[i] === 'o' || thor[i] === 'O') {
-        posiciones_o.push(i);
+for (let key in thor) { // Recorremos la cadena thor.
+    if (thor[key] === 'o' || thor[key] === 'O') { // Vamos comprobando en cada carácter si la letra es igual a o.
+        posiciones_o.push(key); // Si es una o la almacenamos en el array.
     }
 }
 
-alert('Las posiciones de o son: ' + posiciones_o)
+// Mostramos las posiciones en las que se encuentra o.
 
-
+alert('Las posiciones de o son: ' + posiciones_o); // 2,14,19
 
 // Encontrar el carácter que ocupa la posición 3
 
@@ -39,15 +39,32 @@ alert(thor.replace('Thor', 'Loki')); // Loki es el hijo de Odín
 // Recorta la cadena para que devuelva la palabra Odín
 
 let word = 'Odín';
-let word_lenght = word.length;
-let odin = thor.indexOf(word);
-let odin_end = odin + word_lenght;
+let word_lenght = word.length; // Comprobamos tamaño de la palabra que buscamos.
+let odin = thor.indexOf(word); // Buscamos en que posición comienza la palabra que estamos buscando.
+let odin_end = odin + word_lenght; 
+/* A la posición en la que comienza, le sumamos el tamaño de la palabra que buscamos.
 
-alert(thor.substr(odin,word_lenght));
+En este caso la palabra comienza en la posición 19, pues añadimos el tamaño de la palabra que es 4, es decir,
+la palabra que buscamos terminará en la posición 23. */
+
+// Cogemos el string que se encuentre entre las posiciones 19 y 23 ya calculadas.
+// Esto lo hacemos para que en el caso de que añadamos una palabra detrás de Odín, nos siga devolviendo solo Odín.
+
+alert(thor.substr(odin,word_lenght)); // Odín
 
 // ¿Puedes encontrar la letra a? Demuéstralo
 
+let posiciones_a = []; // Creamos un array para guardar las posiciones de o.
 
+for (let key in thor) { // Recorremos la cadena thor.
+    if (thor[key] === 'a' || thor[key] === 'A') { // Vamos comprobando en cada carácter si la letra es igual a a.
+        posiciones_a.push(key); // Si es una a la almacenamos en el array.
+    }
+}
+
+// Mostramos las posiciones en las que se encuentra a.
+
+alert('Las posiciones de a son: ' + posiciones_a); // No mostrará nada debido a que no existe un carácter a en nuestra cadena.
 
 // Pon la mitad de la cadena en minúsculas (Sin usar las posiciones de 0 a 11)
 
