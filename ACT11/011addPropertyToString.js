9677f15f9f9dbe0605a236a83307fbc15dfd6cf1
Mario Rufo Ariza <mariojra6@gmail.com>
2022-11-13 16:54:32 +0100

    Archivos actualizados

--- ACT11/011addPropertyToString.js
+++ ACT11/011addPropertyToString.js
@@ -1,5 +1,16 @@
 "use strict";
 
-let str = "Peter";
+// let str = "Peter";
+// str.test = 5;
+// alert(str.test);
+
+/* No funcionará y no muestra nada debido a que str, al no ser un objeto, str.test no se ha guardado como un elemento de str y no muestra nada.
+
+Para que muestre str.test tendremos que convertir str a objeto y despues añadir str.test = 5: */
+
+let str = {
+    name: 'Peter',
+};
+
 str.test = 5;
-alert(str.test);
+alert(str.test); // 5
