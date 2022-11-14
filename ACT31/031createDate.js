9a21c9562576bf2a319a5a04d281c52f5e92dbf5
Mario Rufo Ariza <mariojra6@gmail.com>
2022-11-14 01:06:29 +0100

    Archivos actualizados

--- ACT31/031createDate.js
+++ ACT31/031createDate.js
@@ -1,1 +1,7 @@
 "use strict";
+
+/* Creamos un objeto Date para la fecha Feb 20, 2012, 3:12 am.
+Usamos este orden en el objeto: año, mes (1, ya que 0 es enero), día, hora, minutos */
+let date = new Date(2012, 1, 20, 3, 12);
+
+alert(date); // Mostramos con un alert.
