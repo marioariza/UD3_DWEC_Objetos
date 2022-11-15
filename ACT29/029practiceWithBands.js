4431fa5a08998648678f7b2880a7f16fae642c4b
Mario Rufo Ariza <mariojra6@gmail.com>
2022-11-15 18:13:26 +0100

    Archivos actualizados

--- ACT29/029practiceWithBands.js
+++ ACT29/029practiceWithBands.js
@@ -9,11 +9,15 @@ let grupos = [
 
 // Filtra los grupos por género obtenido aquellos que son de Heavy Metal.
 
-
+for (let value of grupos) { // Pintamos el array y mostramos sus valores.
+    if (value.genero == 'Heavy Metal') { // Comprobamos que el género sea Heavy Metal
+        alert('Nombre: ' + value.nombre + ' - Género: ' + value.genero); // Si es así, mostramos los nombres de sus grupos.
+    }
+}
 
 // Busca en los grupos usando “find” el grupo con Nombre “Cold Play”.
-
-console.log(grupos.find(name => name.nombre == "Cold Play"));
+let coldplay = grupos.find(name => name.nombre == "Cold Play"); // Buscamos en 'grupos' los que tengan 'Cold Play' como nombre.
+alert('Nombre: ' + coldplay.nombre + ' - Género: ' + coldplay.genero); // Mostramos el grupo con 'Cold Play' como nombre.
 
 // ¿En qué posición del array se encuenta “Cold Play”?
 
